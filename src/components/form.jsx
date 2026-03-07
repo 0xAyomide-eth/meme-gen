378bba412d0b3e6fe1d131b3a3318328b37aef8c
0xAyomide-eth <xayomide.sol@gmail.com>
2026-03-07 17:02:45 +0100

    fetching meme img from API wokrking

--- src/components/form.jsx
+++ src/components/form.jsx
@@ -1,10 +1,21 @@
+import { useState } from "react"
+
 export default function Form() {
+
+    const [meme, Getmeme] = useState(" ")
     //fetch data from the meme api
     const GetMemes = async () => {
         try {
             const response = await fetch("https://api.imgflip.com/get_memes")
             const data = await response.json()
-            console.log(data)
+
+            let memeArray = data.data.memes
+            const random = Math.floor(Math.random() * memeArray.length)
+            Getmeme(memeArray[random].url)
+
+
+
+
         } catch (err) {
             console.log(`Error Occured:${err.message}`)
         }
@@ -26,7 +37,12 @@ export default function Form() {
                     placeholder="Bottom Text"
                 />
                 <button className="FormBtn" onClick={GetMemes}>Generate new meme</button>
+            
 
+            </div>
+
+            <div className="memeImageContainer">
+                 <img src={meme} alt="meme-pic" />
             </div>
         </>
     )
